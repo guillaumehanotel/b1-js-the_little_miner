78959d02fc299eceb19104944ca40a4f45a026a1
guillaumehanotel <guiguihanotel@orange.fr>
2017-05-10 17:59:48 +0200

    séparation des classes

---
+++ js/app/boot.js
@@ -0,0 +1,24 @@
+
+
+    const START_Y = 360;
+    const GAME_WIDTH = 420;
+    const GAME_HEIGHT = 1320;
+    const FRAME_HEIGHT = 650;
+    
+    const PICKER_NB_HIT = 35;
+
+
+    /* const RESISTANCE de chaque type */
+    
+    const RESISTANCE_DIRT = 1;
+    const RESISTANCE_BEDROCK = 9999;
+    const RESISTANCE_STONE = 2;
+    const RESISTANCE_COAL = 1;
+    const RESISTANCE_IRON = 2;
+    const RESISTANCE_GOLD = 3;
+    const RESISTANCE_DIAMOND = 4;
+    const RESISTANCE_TNT = 1;
+    const RESISTANCE_BONUS = 1;
+    const RESISTANCE_DYNAMITE = 1;
+    
+
